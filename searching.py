8690670c6e2797fe685b56a0a510bc08cd946c5d
Agata Vyslouzilova <agata.vyslouzilova@seznam.cz>
2024-04-08 13:16:33 +0200

    message

--- searching.py
+++ searching.py
@@ -19,8 +19,7 @@ def read_data(file_name, field):
         if field == key:
             sequential_data = data[field]
             return sequential_data
-        else:
-            return None
+    return None
 
 
 def linear_search(sekvence, cislo):
@@ -33,11 +32,25 @@ def linear_search(sekvence, cislo):
     return slovnik
 
 
+def pattern_search(sekvence, vzor = ""):
+    pozice = set()
+    delka = len(sekvence)
+    delka_vzoru = len(vzor)
+    for i in range(delka - delka_vzoru + 1):
+        if sekvence[i:i+delka_vzoru] == vzor:
+            pozice.add(i)
+    return pozice
+
+
 def main():
-    sequential_data = read_data("sequential.json", "unordered_numbers")
+    """sequential_data = read_data("sequential.json", "unordered_numbers")
     print("Sequential data: ", sequential_data)
     slovnik = linear_search(sequential_data, 0)
-    print(slovnik)
+    print(slovnik)"""
+    sekvence = read_data("sequential.json", "dna_sequence")
+    print(sekvence)
+    pozice = pattern_search(sekvence, "ATG")
+    print(pozice)
     return
 
 
