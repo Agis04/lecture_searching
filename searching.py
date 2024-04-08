a175195bcad479c38d1c061d21adfe5ff2db462f
Agata Vyslouzilova <agata.vyslouzilova@seznam.cz>
2024-04-08 12:26:47 +0200

    message

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,10 +13,22 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(f"sequential.json") as data_file:
+        allowed_keys = json.load(data_file)["allowed_keys"]
+
+    if field not in allowed_keys:
+        return None
+
+    with open(file_name) as file:
+        data = json.load(file)
+
+    return data.get(field, None)
 
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print("Sequential data: ", sequential_data)
+    return
 
 
 if __name__ == '__main__':
